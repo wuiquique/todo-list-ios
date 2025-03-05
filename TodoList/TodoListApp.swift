6b3e19d8cd2546450c10ea8502826042c7a59e38
wuiquique <luisenriquem15@gmail.com>
2025-03-05 09:52:22 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Luis Menendez on 5/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
